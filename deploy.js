2a968b73192a3c935678148eb7cad9bb51d9a1d2
conqryash007 <guptayash200010@gmail.com>
2021-10-11 22:58:01 +0530

    final commit

--- deploy.js
+++ deploy.js
@@ -6,6 +6,10 @@ const provider = new hdWalletProvider(
   "receive chunk sugar century school coin lyrics fluid vessel december stone forget",
   "https://rinkeby.infura.io/v3/d74a4e3b04cc4061a37770132b6fd0b6"
 );
+//provider is a interface between some etherium network and web3
+//provider with help of truffle
+//Mnemonic from account on metamask
+//deployed on the rinkbey test network
 
 const web3 = new Web3(provider);
 
