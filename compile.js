296b29a34b2feaacccc1d6153d84a7552adfc61b
conqryash007 <guptayash200010@gmail.com>
2021-10-11 12:31:37 +0530

    set up testing environment with mocha

--- compile.js
+++ compile.js
@@ -1,30 +1,30 @@
-//compile.js - for compiling contract using solc i.e. solidity compiler 
+//compile.js - for compiling contract using solc i.e. solidity compiler
 
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname,"contract","Inbox.sol");
-const source = fs.readFileSync(inboxPath,"utf8");
+const inboxPath = path.resolve(__dirname, "contract", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 let input = {
-    language: 'Solidity',
-    sources: {
-        'Inbox.sol' : {
-            content: source
-        }
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
     },
-    settings: {
-        outputSelection: {
-            '*': {
-                '*': [ '*' ]
-            }
-        }
-    }
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
 };
 let output = JSON.parse(solc.compile(JSON.stringify(input)));
 // the compiled code return  1. bytecode 2.ABI
 
-console.log(output.contracts['Inbox.sol']["Inbox"].abi);
+// console.log(output.contracts["Inbox.sol"]["Inbox"].abi);
 
-exports.abi = output.contracts['Inbox.sol']["Inbox"].abi;
-exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+exports.abi = output.contracts["Inbox.sol"]["Inbox"].abi;
+exports.bytecode = output.contracts["Inbox.sol"]["Inbox"].evm.bytecode.object;
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,13 +1,49 @@
-import assert from "assert";
-import ganache from "ganache-cli";
-import Web3 from "web3";//Web3 is a js class
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); //Web3 is a js class
+const { abi, bytecode } = require("./../compile");
 
 //set up a provider
 //provider is a interface between some etherium network and web3
 
-const web3 = new Web3(ganache.provider());// 1. instance of Web3 class
+const web3 = new Web3(ganache.provider()); // 1. instance of Web3 class
 // 2. constructor function called with a provider
 
+//////////////// Basics of Mocha //////////////////
+/*
+class Car {
+  park() {return "stopped"}
+  drive() {return "vroom";}
+}
+let car;
+beforeEach(() => {
+  car = new Car();
+});
 
+describe("car class test", () => {
+  it("can park", () => {
+    assert.strictEqual(car.park(), "stopped");
+});
+  it("can drive", () => {
+    assert.strictEqual(car.drive(), "vroom");
+  });
+});
+*/
+let accounts;
+let inbox;
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
 
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: bytecode,
+      arguments: ["hello"],
+    })
+    .send({ from: accounts[0], gas: 1000000 });
+});
 
+describe("Inbox", () => {
+  it("", () => {
+    console.log(inbox);
+  });
+});
